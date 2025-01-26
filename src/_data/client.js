cd1d595f33ba8765291048065adf670c58dbce48
Sam Robinson <samrobinson@Sams-Mac-mini.home.local>
2025-01-26 15:20:50 -0500

    updated domain

--- src/_data/client.js
+++ src/_data/client.js
@@ -17,5 +17,5 @@ module.exports = {
         "instagram": "https://www.instagram.com/",
     },
     //! Make sure you include the file protocol (e.g. https://) and that NO TRAILING SLASH is included
-    "domain": "https://peakpainting.co"
+    "domain": "https://www.peakpainting.co"
 };
